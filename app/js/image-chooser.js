0f60a5b45a6564c9f98e8903ce714a55096039b2
Anna Andresian <anna1978@gmail.com>
2015-06-29 09:40:01 -0400

    Condense helper functions into setImage function

--- app/js/image-chooser.js
+++ app/js/image-chooser.js
@@ -2,19 +2,13 @@ var allImageNames = ['achilles.jpg','minerva.jpg','caesar.jpg','alexander.jpg','
 var imageElement = document.getElementById('main-image');
 var imageNameBank = [];
 
-var getRandomIndexInArray = function (array) {
-    return Math.floor(Math.random() * array.length);
-}
-
-var getRandomImageName = function () {
-    if (imageNameBank.length === 0) {
-        imageNameBank = imageNameBank.concat(allImageNames);
-    }
-    var index = getRandomIndexInArray(imageNameBank);
-    return imageNameBank.splice(index, 1);
-}
-
 var setImage = function () {
-    var imageName = getRandomImageName();
-    imageElement.src = 'img/' + imageName;
+  if (imageNameBank.length === 0) {
+      imageNameBank = imageNameBank.concat(allImageNames);
+  }
+
+  var index = Math.floor(Math.random() * imageNameBank.length);
+  var imageName = imageNameBank.splice(index, 1);
+
+  imageElement.src = 'img/' + imageName;
 }
