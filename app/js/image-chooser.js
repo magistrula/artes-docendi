851c322867619fdd25dd958a717823a86fa406df
Anna Andresian <anna1978@gmail.com>
2015-06-29 22:08:03 -0400

    Use simplified setImage code

--- app/js/image-chooser.js
+++ app/js/image-chooser.js
@@ -1,20 +1,15 @@
-var allImageNames = ['achilles.jpg','minerva.jpg','caesar.jpg','alexander.jpg','atlas.jpg','scipio.jpg','hercules.jpg'];
-var imageElement = document.getElementById('main-image');
+var allImageNames = ['achilles.jpg','minerva.jpg','caesar.jpg','alexander.jpg',
+                     'atlas.jpg','scipio.jpg','hercules.jpg'];
 var imageNameBank = [];
-
-var getRandomIndexInArray = function (array) {
-    return Math.floor(Math.random() * array.length);
-}
-
-var getRandomImageName = function () {
-    if (imageNameBank.length === 0) {
-        imageNameBank = imageNameBank.concat(allImageNames);
-    }
-    var index = getRandomIndexInArray(imageNameBank);
-    return imageNameBank.splice(index, 1);
-}
+var imageElement = document.getElementById('main-image');
 
 var setImage = function () {
-    var imageName = getRandomImageName();
-    imageElement.src = 'img/' + imageName;
+  if (imageNameBank.length === 0) {
+      imageNameBank = imageNameBank.concat(allImageNames);
+  }
+
+  var index = Math.floor(Math.random() * imageNameBank.length);
+  var imageName = imageNameBank.splice(index, 1);
+
+  imageElement.src = 'img/' + imageName;
 }
