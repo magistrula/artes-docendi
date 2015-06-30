efcab521e114138ca962883b731642ecc6505c65
Anna Andresian <anna1978@gmail.com>
2015-06-29 22:10:33 -0400

    Use simplified setImage function

--- app/js/image-chooser.js
+++ app/js/image-chooser.js
@@ -1,24 +1,18 @@
 var allImageNames = [];
-var imageElement = document.getElementById('main-image');
 var imageNameBank = [];
+var imageElement = document.getElementById('main-image');
 
 $.get('/images/main', function (imageNames) {
   allImageNames = imageNames;    
 });
 
-var getRandomIndexInArray = function (array) {
-    return Math.floor(Math.random() * array.length);
-}
-
-var getRandomImageName = function () {
-    if (imageNameBank.length === 0) {
-        imageNameBank = imageNameBank.concat(allImageNames);
-    }
-    var index = getRandomIndexInArray(imageNameBank);
-    return imageNameBank.splice(index, 1);
-}
-
 var setImage = function () {
-    var imageName = getRandomImageName();
-    imageElement.src = 'img/main/' + imageName;
+  if (imageNameBank.length === 0) {
+      imageNameBank = imageNameBank.concat(allImageNames);
+  }
+
+  var index = Math.floor(Math.random() * imageNameBank.length);
+  var imageName = imageNameBank.splice(index, 1);
+
+  imageElement.src = 'img/main/' + imageName;
 }
