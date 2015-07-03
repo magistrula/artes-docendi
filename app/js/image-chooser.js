229cb5dec76838721d5fb7607b9942d45a24fae2
Anna Andresian <anna1978@gmail.com>
2015-07-02 23:04:11 -0400

    Update setImage function to match earlier branches

--- app/js/image-chooser.js
+++ app/js/image-chooser.js
@@ -5,11 +5,11 @@ var imageElement = document.getElementById('main-image');
 
 var setImage = function () {
   if (imageNameBank.length === 0) {
-      imageNameBank = imageNameBank.concat(allImageNames);
+    imageNameBank = imageNameBank.concat(allImageNames);
   }
 
   var index = Math.floor(Math.random() * imageNameBank.length);
-  var imageName = imageNameBank.splice(index, 1);
+  var imageName = imageNameBank.splice(index, 1)[0];
 
   imageElement.src = 'img/' + imageName;
 }
