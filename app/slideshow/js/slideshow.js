18bd4983836070c015e2d0b610de8f88be6dc7a9
Anna Andresian <anna@magistrula.com>
2016-06-22 20:52:45 -0700

    Convert slideshow.js to es5

--- app/slideshow/js/slideshow.js
+++ app/slideshow/js/slideshow.js
@@ -1,7 +1,7 @@
-let imageManager;
+var imageManager;
 
 function initialize() {
-  $.get('/slideshow_images', (imagePaths) => {
+  $.get('/slideshow_images', function(imagePaths) {
     imageManager = new ImageManager('main-image', imagePaths);
     $('.main-image-button').attr('disabled', false);
   });
