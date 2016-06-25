c3a794d381fc3659a98b0853ef87a2baf6ebba8b
Anna Andresian <anna@magistrula.com>
2016-06-25 12:45:57 -0700

    Preload images

--- app/slideshow/js/image-manager.js
+++ app/slideshow/js/image-manager.js
@@ -1,8 +1,15 @@
 var ImageManager = function(imageElementId, imagePaths) {
   this.imageElement = document.getElementById(imageElementId);
   this.imageChooser = new ItemChooser(imagePaths);
+  _loadImages(imagePaths);
 };
 
 ImageManager.prototype.setImage = function() {
   this.imageElement.src = this.imageChooser.chooseItem();
+}
+
+function _loadImages(imagePaths) {
+  imagePaths.forEach(function(path) {
+    $.get(path);
+  });
 }
